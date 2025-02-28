09d5cb39c825d0df377f5510c92b34ccaff889c9
Aurorn <aurornispro@gmail.com>
2025-02-28 15:09:52 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Wheel from './components/Wheel';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Spin the Wheel</h1>
+      <Wheel />
+    </div>
+  );
+}
